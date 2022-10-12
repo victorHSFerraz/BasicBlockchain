b56956fa540f3a346e081ee0b55d35838e7b680d
victorHSFerraz <victorf178@gmail.com>
2022-10-12 20:03:11 -0300

    dynamic difficulty

--- blockchain/index.js
+++ blockchain/index.js
@@ -9,7 +9,7 @@ class Blockchain {
     const lastBlock = this.chain[this.chain.length - 1];
     const block = Block.mineBlock(lastBlock, data);
     this.chain.push(block);
-
+    console.log("addBlock", block.toString());
     return block;
   }
 
@@ -20,7 +20,6 @@ class Blockchain {
     for (let i = 1; i < chain.length; i++) {
       const block = chain[i];
       const lastBlock = chain[i - 1];
-
       if (
         block.lastHash !== lastBlock.hash ||
         block.hash !== Block.blockHash(block)
