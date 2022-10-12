43f276fcf44e881302ae671f7f4f9e545fdfb780
victorHSFerraz <victorf178@gmail.com>
2022-10-11 23:13:23 -0300

    p2p listen

--- app/index.js
+++ app/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const Blockchain = require("../blockchain");
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
+const P2pServer = require("./p2p-server");
 
 const app = express();
 const bc = new Blockchain();
+const p2pServer = new P2pServer(bc);
 
 app.use(express.json());
 
@@ -19,3 +21,4 @@ app.post("/mine", (req, res) => {
 });
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
+p2pServer.listen();
