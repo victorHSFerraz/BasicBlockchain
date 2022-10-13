5fb5d6ba9333bbb77313b63844a43a55e3a7b0b3
victorHSFerraz <victorf178@gmail.com>
2022-10-13 19:51:10 -0300

    keys

--- blockchain/index.js
+++ blockchain/index.js
@@ -9,7 +9,6 @@ class Blockchain {
     const lastBlock = this.chain[this.chain.length - 1];
     const block = Block.mineBlock(lastBlock, data);
     this.chain.push(block);
-    console.log("addBlock", block.toString());
     return block;
   }
 
