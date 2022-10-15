c185f9a2eacbc5b6f1626bdb8dcea37898c91ae9
victorHSFerraz <victorf178@gmail.com>
2022-10-15 11:49:06 -0300

    update transaction

--- blockchain/block.js
+++ blockchain/block.js
@@ -1,4 +1,4 @@
-const SHA256 = require("crypto-js/sha256");
+const ChainUtil = require("../chain-util/chain-util");
 const { DIFFICULTY, MINE_RATE } = require("../config");
 
 class Block {
@@ -41,7 +41,7 @@ class Block {
   }
 
   static hash(timestamp, lastHash, data, nonce, difficulty) {
-    return SHA256(
+    return ChainUtil.hash(
       `${timestamp}${lastHash}${data}${nonce}${difficulty}`
     ).toString();
   }
--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -23,6 +23,19 @@ describe("Transaction", () => {
     ).toEqual(amount);
   });
 
+  it("inputs the balance of the wallet", () => {
+    expect(transaction.input.amount).toEqual(wallet.balance);
+  });
+
+  it("validates a valid transaction", () => {
+    expect(Transaction.verifyTransaction(transaction)).toEqual(true);
+  });
+
+  it("invalidates a corrupted transaction", () => {
+    transaction.outputs[0].amount = 5000;
+    expect(Transaction.verifyTransaction(transaction)).toEqual(false);
+  });
+
   describe("transaction that exceeds the balance", () => {
     beforeEach(() => {
       amount = 500000;
@@ -31,6 +44,30 @@ describe("Transaction", () => {
 
     it("does not create the transaction", () => {
       expect(transaction).toEqual(undefined);
+    });
+  });
+
+  describe("updating a transaction", () => {
+    let nextAmount, nextRecipient;
+
+    beforeEach(() => {
+      nextAmount = 20;
+      nextRecipient = "ps5y4biu423bx";
+      transaction = transaction.update(wallet, nextRecipient, nextAmount);
+    });
+
+    it("subtracts the next amount from the sender output", () => {
+      expect(
+        transaction.outputs.find((output) => output.address == wallet.publicKey)
+          .amount
+      ).toEqual(wallet.balance - amount - nextAmount);
+    });
+
+    it("outputs an amount for the next recipient", () => {
+      expect(
+        transaction.outputs.find((output) => output.address == nextRecipient)
+          .amount
+      ).toEqual(nextAmount);
     });
   });
 });
